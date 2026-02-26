4884023d0d8ab99b9d340d9ff536d650a74815a6
Maxi Khevenhüller <maxi.khevenhueller@gmail.com>
2026-02-26 16:16:52 +0100

    Prepare rails app with external specs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,27 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Movie.destroy_all
+Movie.create!([
+  {
+    title: "Psycho",
+    overview: "A Phoenix secretary embezzles $40,000 from her employer's client, goes on the run, and checks into a remote motel run by a young man under the domination of his mother.",
+    poster_url: "https://image.tmdb.org/t/p/original/yz49qB2v8Q7STBh4H68P9zwE6v.jpg",
+    rating: 8.5
+  },
+  {
+    title: "Rear Window",
+    overview: "A wheelchair-bound photographer spies on his neighbors from his Greenwich Village courtyard apartment window, and becomes convinced one of them has committed murder.",
+    poster_url: "https://image.tmdb.org/t/p/original/ILC99xyYn569u0p9tNNo67z87i.jpg",
+    rating: 8.5
+  },
+  {
+    title: "Vertigo",
+    overview: "A former San Francisco police detective juggles wrestling with his personal demons and becoming obsessed with the hauntingly beautiful woman he has been hired to trail.",
+    poster_url: "https://image.tmdb.org/t/p/original/15uOE6TV76VFVs872v3tqnM716y.jpg",
+    rating: 8.3
+  },
+  {
+    title: "North by Northwest",
+    overview: "A New York City advertising executive is mistaken for a government agent by a group of foreign spies, and is pursued across the country.",
+    poster_url: "https://image.tmdb.org/t/p/original/8Yba3YmSj77S6S7pY7S9p8S7p8S.jpg",
+    rating: 8.3
+  }
+])
